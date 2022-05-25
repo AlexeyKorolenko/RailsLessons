70932c6c7ae3e6436da526e6babad85ea3254448
alexeykorolenko.ab@gmail.com <alexeykorolenko.ab@gmail.com>
2022-05-25 13:14:45 +0300

    one to many

--- askit/config/routes.rb
+++ askit/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :questions
   # resources - позволяет создать множество маршрутов для одного контроллера
-  # delete '/questions/:id', to: 'questions#destroy'
+  resources :questions do
+    resources :answers, only: %i[create destroy update edit]
+  end
   # pages - название контроллера, который будет обрабатывать входящий запрос
   # index - метод который будет отвечать на запрос
   root 'pages#index'
