b4c853430553f8886fc30e7ea61f145217c00d35
alexeykorolenko.ab@gmail.com <alexeykorolenko.ab@gmail.com>
2022-05-25 11:46:03 +0300

    first commit

--- askit/app/controllers/questions_controller.rb
+++ askit/app/controllers/questions_controller.rb
@@ -1,16 +1,15 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[show destroy edit update]
 
   def show
-    @question = Question.find_by id: params[:id]
   end
 
   def edit
-    @question = Question.find_by id: params[:id]
   end
 
   def update
-    @question = Question.find_by id: params[:id]
     if @question.update question_params
+      flash[:success] = "Question updated!"
       redirect_to questions_path
     else
       render :edit
@@ -28,6 +27,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new question_params
     if @question.save
+      flash[:success] = "Question created!"
       redirect_to questions_path
     else
       render :new
@@ -36,8 +36,8 @@ class QuestionsController < ApplicationController
 
   def destroy
     #@question = Question.find_by id: params[:id]
-    @question = Question.find(params[:id])
     @question.destroy
+    flash[:success] = "Question deleted!"
     redirect_to questions_path
   end
 
@@ -45,5 +45,9 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:title, :body)
+  end
+
+  def set_question
+    @question = Question.find params[:id]
   end
 end
